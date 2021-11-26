c359b3fcd8ae085d191cf2870a2a18e149d739f1
DeeDoubleYous <wishart.a470@gmail.com>
2021-11-26 11:59:47 +0000

    The map box shows up now

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -6,27 +6,30 @@ import * as L from 'leaflet';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
   constructor() { }
 
   map?: L.Map;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.initMap();
   }
 
   initMap():void{
     this.map = L.map('map');
+
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}'
     +           '?access_token={accessToken}', {
-      attribution: 'Map data &copy; OpenstreetMap |'
+      attribution: 'Map data &copy; OpenstreetMap | '
                   + 'Map tiles &copy; Mapbox',
       tileSize: 512,
       maxZoom: 18,
       zoomOffset: 18,
-      id: 'mapbox/streets-v11',
+      id: 'mapbox/dark-v11',
       accessToken: 'pk.eyJ1IjoiYWxleC13aXNoYXJ0IiwiYSI6ImNrdWNoczhqbTEwZ2EycHF2aTM5OHA4MGcifQ.RVRYKZRa6CRVJ1I6ui1ogg'
     }).addTo(this.map);
+
+    this.map.setView([50.845388, -0.118508], 10);
   }
 }
